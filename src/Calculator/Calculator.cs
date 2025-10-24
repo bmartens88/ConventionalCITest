6c34f47dfe45354d276f43cd89f15e4f30cd8638
Bas Martens <bas.martens.1988@outlook.com>
2025-10-24 23:08:40 +0200

    feat: added application and test code
    fix(ci): updated ci.yml for building and testing project

---
+++ src/Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator;
+
+public static class Calculator
+{
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Min(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
